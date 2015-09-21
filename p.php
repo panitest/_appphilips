c99d2e56ef13c457fee8bd5735fa56af51ad357b
Andrés Ricardo Fernández <mobius@disegnocentell.com.ar>
2015-09-21 13:50:43 -0300

    mch

--- p.php
+++
@@ -1,8 +0,0 @@
-<?php
-$content=file_get_contents('http://www.disegnocentell.com.ar/phillips/');
-$pattern='/<html>(.*?)<\/html>/is';
-preg_match_all($pattern,$content,$result);
-?>
-<?php
-echo 'document.write(\''.preg_replace( "/\r|\n/", "",str_replace("'",'&quot;',$result[1][0])).'\')';
-?>
